30abb7ef3b30340b60d71c00245e3b02de0dbff1
Quantum <quantum2048@gmail.com>
2015-04-13 17:45:58 -0400

    Added caching support.

--- mathserver.js
+++ mathserver.js
@@ -3,6 +3,9 @@
 var mjAPI = require('MathJax-node/lib/mj-single.js');
 var url = require('url');
 var unescape = require('querystring').unescape;
+var fs = require('fs');
+var path = require('path');
+var crypto = require('crypto');
 
 var argv = require('yargs')
   .demand(1).strict()
@@ -27,6 +30,10 @@ var argv = require('yargs')
       default: '/display_math',
       describe: 'web font to use'
     },
+    cache: {
+      default: 'cache',
+      describe: 'cache directory'
+    }
   })
   .argv;
 
@@ -44,13 +51,24 @@ if (!argv.no_optimize) {
 } else
   var svgo = undefined;
 
+var cache_dir = argv.cache;
 var urlmap = {};
 urlmap[argv.inline_path] = {
-  format: 'inline-TeX',
+  format: 'inline-TeX', cache: 'svgi', content_type: 'image/svg+xml; charset=utf-8'
 };
 urlmap[argv.display_path] = {
-  format: 'TeX',
+  format: 'TeX', cache: 'svgd', content_type: 'image/svg+xml; charset=utf-8'
 };
+
+function save(file, data) {
+  var temp = file + '.tmp';
+  fs.writeFile(temp, data, function (err) {
+    if (err) console.log(err);
+    fs.rename(temp, file, function (err) {
+      if (err) console.log(err);
+    });
+  });
+}
 
 require('http').createServer(function (req, res) {
   var params = url.parse(req.url);
@@ -61,21 +79,43 @@ require('http').createServer(function (req, res) {
   }
   var config = urlmap[params.pathname];
   var math = unescape(params.query);
-  mjAPI.typeset({
-    math: math,
-    format: config.format,
-    svg: true,
-  }, function (data) {
-    if (data.errors) {
-      res.writeHead(400, {'Content-Type': 'text/plain'});
-      res.end(data.errors);
-    } else {
-      res.writeHead(200, {'Content-Type': 'image/svg+xml; charset=utf-8'});
-      if (typeof svgo !== 'undefined') {
-        svgo.optimize(data.svg, function (result) {
-          res.end(result.data);
-        });
-      } else res.end(data.svg);
+  var hash = crypto.createHash('md5').update(math).digest('hex');
+  var cache = path.join(cache_dir, config.cache + '_' + hash);
+  fs.exists(cache, function (exists) {
+    if (exists) {
+      fs.readFile(cache, function (err, data) {
+        if (err) {
+          console.log(err);
+          res.writeHead(500, {'Content-Type': 'text/plain'});
+          res.end(err);
+        } else {
+          res.writeHead(200, {'Content-Type': config.content_type});
+          res.end(data);
+        }
+      });
+      return;
     }
+    mjAPI.typeset({
+      math: math,
+      format: config.format,
+      svg: true,
+    }, function (data) {
+      if (data.errors) {
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.end(data.errors);
+      } else {
+        if (typeof svgo !== 'undefined') {
+          svgo.optimize(data.svg, function (result) {
+            res.writeHead(200, {'Content-Type': config.content_type});
+            res.end(result.data);
+            save(cache, result.data);
+          });
+        } else {
+          res.writeHead(200, {'Content-Type': config.content_type});
+          res.end(data.svg);
+          save(cache, data.svg);
+        }
+      }
+    });
   });
 }).listen(parseInt(argv._[0]), argv.host);
